b67fac256a57a8ca796d9c42c7e35789fa130fcd
nutanBhakuni <sbhakuni.bhakuni@gmail.com>
2018-01-15 15:33:25 +0530

    Update show_local_weather.js

--- show_local_weather.js
+++ show_local_weather.js
@@ -19,15 +19,13 @@
     let longi = position.coords.longitude;
     let lati = position.coords.latitude;
     
-    para.innerHTML = "Longitude: " + longi + " " + "Latitude: " + lati;
-    
     fetch('https://api.apixu.com/v1/current.json?key=aba9f3d569dd440cb8692516181501&q='+lati+','+longi)
       .then(function(response){
         return response.json();
     }).then(function(data){
       //let image = document.querySelector('img');
       //image.src = data.weather[0].icon;
-      console.log(data);
+      para.innerHTML = data.current.temp_c;
     });
   }
   
